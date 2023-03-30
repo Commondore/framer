de7440c866781fe9025ab02b917513f2426c620b
Alex Som <alex.som86@gmail.com>
2023-03-30 10:35:49 +0600

    animate on scroll

--- src/components/Examples/Example6.tsx
+++ src/components/Examples/Example6.tsx
@@ -1,10 +1,31 @@
 import { BoxAnimate } from '@/components/Examples/BoxAnimate'
 import { Title } from '@/components/Examples/styled'
+import { motion } from 'framer-motion'
+
+import styles from './style.module.scss'
 
 export const Example6 = () => {
   return (
     <BoxAnimate>
       <Title>Анимация по скроллу</Title>
+
+      {Array.from(Array(5), (_, index) => {
+        return (
+          <motion.div
+            key={index}
+            className={styles.block}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, transition: { duration: 1 } }}
+          >
+            <motion.h3
+              initial={{ opacity: 0, y: 200 }}
+              whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+            >
+              Блок номер {index + 1}
+            </motion.h3>
+          </motion.div>
+        )
+      })}
     </BoxAnimate>
   )
 }
